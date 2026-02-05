415276b1eed247fe126989ed788527011b9f37a8
Abdelkader MANNAI <abdelkadermannai87@gmail.com>
2026-02-06 00:27:45 +0100

    chore: initialize package structure

---
+++ backend/src/main/java/com/mannai/stageflow/StageflowApplication.java
@@ -0,0 +1,13 @@
+package com.mannai.stageflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StageflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StageflowApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mannai/stageflow/StageflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mannai.stageflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StageflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
